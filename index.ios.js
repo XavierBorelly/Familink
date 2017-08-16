a8067011799d064683f6d399521adabe77ef1042
xavier borelly <>
2017-08-16 14:31:45 +0200

    ajout de la regle lint sur les brace-styles

--- index.ios.js
+++ index.ios.js
@@ -5,15 +5,18 @@
  */
 
 import React, { Component } from 'react';
-import {
+import
+{
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-export default class familink extends Component {
-  render() {
+export default class familink extends Component
+{
+  render()
+  {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -31,23 +34,27 @@ export default class familink extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const styles = StyleSheet.create(
+  {
+    container:
+    {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome:
+    {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions:
+    {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('familink', () => familink);
