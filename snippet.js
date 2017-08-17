2144556ef3bded81cfaa93a247273edc0be26037
Guillaume Battez <guillaume.battez@hotmail.fr>
2017-08-17 13:55:16 +0200

    Ajout de la classe Popin

--- snippet.js
+++
@@ -1,9 +0,0 @@
-Alert.alert(
-  'Voulez-vous vraiment (action) ?', '',
-  [
-    {text: 'Annuler', onPress: () => console.log('Annuler Pressed')},
-    {text: ''},
-    {text: 'OK', onPress: () => console.log('OK Pressed')},
-  ],
-  { cancelable: false }
-)
