a92e63d83dc3ee9ee2059eb64004e64c09e31c33
Donovan Chabrat <donovanchabrat@hotmail.fr>
2017-08-24 16:35:30 +0200

    Début page Profil avec un switch de vue entre lecture et update.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import BackButton from '../components/BackButton';
 import Header from '../components/Header';
 import { HOME_SCENE_NAME } from './HomeScreen';
-import MenuIcon from '../../assets/icon_profile.jpg';
-import BackButton from '../components/BackButton';
+import UpdateProfil from '../components/UpdateProfil';
 
 export const PROFILE_SCENE_NAME = 'PROFILE_SCENE';
-
 const $bgColor = '#F5FCFF';
 
 const styles = StyleSheet.create({
@@ -17,31 +15,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: $bgColor,
   },
-  icon: {
-    width: 48,
-    height: 48,
-  },
 });
-
-export default class ProfileScreen extends Component
+export default class SignUpScreen extends Component
 {
   static navigationOptions = {
     drawerLabel: 'Mon Profil',
-    drawerIcon: (<Image source={MenuIcon} style={[styles.icon]} />),
   };
-
   render()
   {
     const navigation = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Header hasMenu navigation={navigation} title="Mon profil" />
+        <Header navigation={navigation} title="Mon Profil" />
         <BackButton navigation={navigation} param={HOME_SCENE_NAME} />
+        <UpdateProfil />
       </View>
     );
   }
 }
-
-ProfileScreen.propTypes = {
+SignUpScreen.propTypes = {
   navigation: React.PropTypes.objectOf(React.PropTypes.any).isRequired,
 };
