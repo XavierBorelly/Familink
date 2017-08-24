ccafe5fb2b8ee4cd3f5ec0e1066652063cc336f1
Donovan Chabrat <donovanchabrat@hotmail.fr>
2017-08-24 16:44:34 +0200

    retour du burger Menu

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import BackButton from '../components/BackButton';
 import Header from '../components/Header';
 import { HOME_SCENE_NAME } from './HomeScreen';
+import MenuIcon from '../../assets/icon_profile.jpg';
 import UpdateProfil from '../components/UpdateProfil';
 
 export const PROFILE_SCENE_NAME = 'PROFILE_SCENE';
+
 const $bgColor = '#F5FCFF';
 
 const styles = StyleSheet.create({
@@ -15,24 +17,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: $bgColor,
   },
+  icon: {
+    width: 48,
+    height: 48,
+  },
 });
-export default class SignUpScreen extends Component
+export default class ProfileScreen extends Component
 {
   static navigationOptions = {
     drawerLabel: 'Mon Profil',
+    drawerIcon: (<Image source={MenuIcon} style={[styles.icon]} />),
   };
   render()
   {
     const navigation = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Header navigation={navigation} title="Mon Profil" />
+        <Header has Menu navigation={navigation} title="Mon Profil" />
         <BackButton navigation={navigation} param={HOME_SCENE_NAME} />
         <UpdateProfil />
       </View>
     );
   }
 }
-SignUpScreen.propTypes = {
+ProfileScreen.propTypes = {
   navigation: React.PropTypes.objectOf(React.PropTypes.any).isRequired,
 };
