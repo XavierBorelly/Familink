40e71efe1136ede3a33a5a86c857455a5f661c0c
xavier borelly <>
2017-08-25 16:29:07 +0200

    Début de la création de la liste de contact

--- src/screens/PhonebookScreen.js
+++ src/screens/PhonebookScreen.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList, TouchableHighlight, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 import { CONTACT_SCENE_NAME } from '../apps/ContactApp';
 import { HOME_SCENE_NAME } from './HomeScreen';
 import BackButton from '../components/BackButton';
 import Header from '../components/Header';
+import iconCall from '../../assets/icon_phone.jpg';
+import defaultGravatar from '../../assets/icon_defaultGravatar.jpg';
 import Contact from '../models/Contact';
 
 export const PHONEBOOK_SCENE_NAME = 'PHONEBOOK_SCENE';
 
 const $bgColor = '#F5FCFF';
+
+const contact1 = new Contact(null, '0606060606', 'granny', 'lastName', 'email@email.com', 'SENIOR', 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50', 'true', 'true');
+const contact2 = new Contact(null, '0606060606', 'granny', 'lastName', 'email@email.com', 'SENIOR', null, 'true', 'true');
+const contact3 = new Contact(null, '0606060606', 'granny', 'lastName', 'email@email.com', 'SENIOR', 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50', null, null);
+const contact4 = new Contact(null, '0606060606', 'granny', 'lastName', 'email@email.com', 'SENIOR', null, null, null);
+
+const contact = [contact1, contact2, contact3, contact4, contact1, contact2, contact3, contact4];
 
 const styles = StyleSheet.create({
   container: {
@@ -19,27 +28,71 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: $bgColor,
   },
+  itemContactContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: 70,
+    alignItems: 'center',
+    margin: 10,
+    borderWidth: 1,
+  },
+  textItemContactContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 // eslint-disable-next-line react/prefer-stateless-function
-export class PhonebookScreen extends Component
+export default class PhonebookScreen extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = { };
+  }
+
+  componentDidMount()
+  {
+    console.log(contact);
+  }
+
+  navigateToInfo(trailer) {
+    this.navigate(INFO_SCENE_NAME, {contact : contact});
+  };
+
   render()
   {
     const navigation = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Header hasMenu navigation={navigation} title="Répertoire" />
-        <Text>Contacts in store : {this.props.contactsList.length} </Text>
-        {/* <Text>{JSON.stringify(this.props.contactsList)}</Text> */}
-        <Button
-          onPress={() =>
-          {
-            navigation.navigate(CONTACT_SCENE_NAME, new Contact(0, {}));
-          }
-          }
-          title="Ajouter contact"
+      <View >
+        <View style={styles.container}>
+          <Header hasMenu navigation={navigation} title="Répertoire" />
+        </View>
+        <FlatList
+          data={contact}
+          renderItem={({ item }) =>
+            (<TouchableHighlight onPress={() => this.navigateToInfo(item)}>
+            <View style={styles.itemContactContainer}>
+              <Image source={item.gravatar === null || item.gravatar === '' ? defaultGravatar : {uri: item.gravatar} } style={{width: 70, height: 70}}  />
+              <View style={styles.textItemContactContainer}>
+                <View>
+                  <Text>{item.firstName} {item.lastName}</Text>
+                </View>
+                <View>
+                  <Text>{item.isFamilinkUser} {item.isEmergencyUser}</Text>
+                </View>
+              </View>
+                <TouchableHighlight onPress={() => alert('call en cours')}>
+                  <Image style={{width: 70, height: 70}} source={iconCall} />
+                </TouchableHighlight>
+            </View>
+
+            </TouchableHighlight>)}
         />
+
         <BackButton navigation={navigation} param={HOME_SCENE_NAME} />
       </View>
     );
@@ -48,16 +101,4 @@ export class PhonebookScreen extends Component
 
 PhonebookScreen.propTypes = {
   navigation: React.PropTypes.objectOf(React.PropTypes.any).isRequired,
-  contactsList: React.PropTypes.arrayOf(Contact).isRequired,
 };
-
-// Map props from redux state
-function mapStateToProps(state)
-{
-  return {
-    contactsList: state.contactsReducer.contactsList,
-  };
-}
-
-// connect screen elements to redux object
-export default connect(mapStateToProps, null)(PhonebookScreen);
