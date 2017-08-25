60cf85d24808a9462b4121ca0f35ee86e097cc00
xavier borelly <>
2017-08-25 11:12:22 +0200

    Revert "suppression d'un fichier devenu inutile"
    This reverts commit 93b3da61b44713ca62751be22b937ca2b77da395.

---
+++ src/models/Contact.js
@@ -0,0 +1,15 @@
+export default class Contact
+{
+  constructor(
+    id, phone, firstName, lastName, email, profile, isFamilinkUser, isEmergencyUser)
+  {
+    this.id = id;
+    this.phoneNumber = phone;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.profile = profile;
+    this.isFamilinkUser = isFamilinkUser;
+    this.isEmergencyUser = isEmergencyUser;
+  }
+}
