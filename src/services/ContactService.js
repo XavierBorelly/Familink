b65e4a8aa43ddcbd3dae5d368642c9be63c917e1
Arnaud L'her <arnaud.lher@laposte.net>
2017-08-28 11:37:12 +0200

    Création de la classe vierge ContactService

---
+++ src/services/ContactService.js
@@ -0,0 +1,8 @@
+export default class ContactService
+{
+  constructor()
+  {
+    // eslint-disable-next-line
+    console.log('Construction de la classe ContactService gérant les contacts et leurs traitements');
+  }
+}
