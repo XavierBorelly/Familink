c0dfc183b3b363c31e36f990b567ed4a601866df
Donovan Chabrat <donovanchabrat@hotmail.fr>
2017-08-29 16:03:05 +0200

    création barre de Recherche

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+import React, { Component, PropTypes } from 'react';
+import { Container, Header, Item, Input, Icon } from 'native-base';
+import _ from 'lodash';
 
-const SearchBar = () =>
-  (
-    <Container>
-      <Header searchBar rounded>
-        <Item>
-          <Icon name="ios-search" />
-          <Input placeholder="Search" />
-          <Icon name="ios-people" />
-        </Item>
-        <Button transparent>
-          <Text>Search</Text>
-        </Button>
-      </Header>
-    </Container>
-  );
-export default SearchBar;
+export default class SearchBar extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.onCurrentSearchChange = this.onCurrentSearchChange.bind(this);
+    this.state = { listContacts: props.arrayContacts };
+  }
+
+  onCurrentSearchChange(text)
+  {
+    this.props.onChange(item =>
+      (item.firstName.contains(text) || item.lastName.contains(text)));
+
+
+    const filterList = _.filter(this.state.listContacts, item =>
+      (item.firstName.contains(text) || item.lastName.contains(text)));
+    return filterList;
+  }
+
+  render()
+  {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input
+              placeholder="Search"
+              onChangeText={text => this.onCurrentSearchChange(text)}
+            />
+            <Icon name="ios-people" />
+          </Item>
+        </Header>
+      </Container>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  arrayContacts: PropTypes.objectOf(PropTypes.any).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
