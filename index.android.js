6cc3d4a7fe9531dd35a308c78ca0ed39b3d028bd
Donovan Chabrat <donovanchabrat@hotmail.fr>
2017-08-30 17:38:48 +0200

    Test d'une méthode, mais warnings

--- index.android.js
+++ index.android.js
@@ -5,22 +5,22 @@
  */
 
 import React from 'react';
-import NetInfo, { AppRegistry } from 'react-native';
+import { NetInfo, AppRegistry } from 'react-native';
 
 import ApplicationNavigator from './src/AppNavigator';
 import { handleFirstConnectivityChange } from './src/errors/Token';
 
 const familink = () =>
 {
+  NetInfo.isConnected.addEventListener( // faaire l'appel dans index.ios et index.android
+    'change',
+    handleFirstConnectivityChange,
+  );
   NetInfo.isConnected.fetch().then((isConnected) =>
   {
-    NetInfo.isConnected.addEventListener( // faaire l'appel dans index.ios et index.android
-      'change',
-      handleFirstConnectivityChange,
-    );
-    handleFirstConnectivityChange(isConnected, props);
+    handleFirstConnectivityChange(isConnected);
   });
-  <ApplicationNavigator />;
+  return <ApplicationNavigator />;
 };
 
 console.ignoredYellowBox = ['Warning:'];
--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,23 @@
  */
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { NetInfo, AppRegistry } from 'react-native';
 
 import ApplicationNavigator from './src/AppNavigator';
+import { handleFirstConnectivityChange } from './src/errors/Token';
 
-const familink = () => <ApplicationNavigator />;
+const familink = () =>
+{
+  NetInfo.isConnected.addEventListener( // faaire l'appel dans index.ios et index.android
+    'change',
+    handleFirstConnectivityChange,
+  );
+  NetInfo.isConnected.fetch().then((isConnected) =>
+  {
+    handleFirstConnectivityChange(isConnected);
+  });
+  return <ApplicationNavigator />;
+};
 
 console.ignoredYellowBox = ['Warning:'];
 
