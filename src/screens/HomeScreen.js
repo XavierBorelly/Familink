0597e49d6ffb5328f3d88b12b53eedcd3b2f4bdd
XavierBorelly <borelly.xavier@gmail.com>
2017-09-01 10:57:34 +0200

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,7 +5,7 @@ import { PROFILE_SCENE_NAME } from './ProfileScreen';
 import { PHONEBOOK_SCENE_NAME } from '../apps/PhonebookApp';
 import MenuIcon from '../../assets/icon_home.png';
 import familinkStyles from '../Style';
-import { headerHome } from '../Util';
+import { headerHome, headerContact} from '../Util';
 
 export const HOME_SCENE_NAME = 'HOME_SCENE';
 
@@ -49,7 +49,7 @@ export default class HomeScreen extends Component
               }
               }
             >
-              <Text style={familinkStyles.buttonText}>Repertoire</Text>
+              <Text style={familinkStyles.buttonText}>{headerContact}</Text>
             </TouchableHighlight>
           </View>
         </View>
