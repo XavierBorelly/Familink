8622fd4ef194e526618896890806f2da79c2b6ad
XavierBorelly <borelly.xavier@gmail.com>
2017-09-06 10:03:40 +0200

    Bug fix

--- src/service/ContactService.js
+++ src/service/ContactService.js
@@ -14,10 +14,10 @@ export default class ContactService
   // Transforme l'objet JSON du serveur pour manipuler des objets Contact
   static mapToContactModel(obj)
   {
-    const OptionalAttributes = new OptionalAttributes(obj.lastName, obj.email, obj.gravatar,
+    const optionalAttributes = new OptionalAttributes(obj.lastName, obj.email, obj.gravatar,
       obj.isFamilinkUser, obj.isEmergencyUser);
     // eslint-disable-next-line no-underscore-dangle
-    return new Contact(obj._id, obj.phone, obj.firstName, obj.profile, OptionalAttributes);
+    return new Contact(obj._id, obj.phone, obj.firstName, obj.profile, optionalAttributes);
   }
 
   static getAllContacts()
